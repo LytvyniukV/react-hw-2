47e34d8806989d482260234f67f6b3e7eb7f045e
Vladyslav Lytvyniuk <lytvyniuk.vladyslav@gmail.com>
2024-02-12 13:14:28 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Description } from './Description/Description';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Options } from './Options/Options';
 import { Feedback } from './Feedback/Feedback';
 function App() {
@@ -15,25 +15,16 @@ function App() {
       ...feedbacks,
       [feedback]: feedbacks[feedback] + 1,
     });
-  };
-  useEffect(() => {
     setTotalFeedback(feedbacks.good + feedbacks.neutral + feedbacks.bad);
-  }, [feedbacks.good, feedbacks.neutral, feedbacks.bad]);
-
-  useEffect(() => {
     setPositivePercent(
       Math.round(((feedbacks.good + feedbacks.neutral) / totalFeedback) * 100)
     );
-  }, [feedbacks.neutral, feedbacks.good]);
+  };
 
   return (
     <>
       <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        totalFeedback={totalFeedback}
-        feedBack={feedbacks}
-      />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
       <Feedback
         positivePercent={positivePercent}
         totalFeedback={totalFeedback}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -6,12 +6,18 @@ export const Feedback = ({
   feedbacks: { good, neutral, bad },
 }) => {
   return (
-    <div className={css.container}>
-      <p className={css.text}>Good: {good}</p>
-      <p className={css.text}>Neutral: {neutral}</p>
-      <p className={css.text}>Bad: {bad}</p>
-      <p className={css.text}>Total: {totalFeedback}</p>
-      <p className={css.text}>Positive: {positivePercent}</p>
-    </div>
+    <>
+      {totalFeedback === 0 ? (
+        <p className={css.text}>No feedback yet</p>
+      ) : (
+        <div className={css.container}>
+          <p className={css.text}>Good: {good}</p>
+          <p className={css.text}>Neutral: {neutral}</p>
+          <p className={css.text}>Bad: {bad}</p>
+          <p className={css.text}>Total: {totalFeedback}</p>
+          <p className={css.text}>Positive: {positivePercent}</p>
+        </div>
+      )}
+    </>
   );
 };
